ade5eb9cec8d676e4e76010f662f2eae983dcdc2
Annen3121 <42419439+Annen3121@users.noreply.github.com>
2019-02-04 17:46:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,13 +6,7 @@ var fs = require('fs');
 // -->COMMANDS<--
 
 bot.on('guildMemberAdd',member =>{
-    member.send({embed: {
-  color: 0x36393e,
-  footer: { 
-    text: 'Katılmayı unutma!'
-    },
-  description: `**Merhaba! Nasılsın dostum? Sana kısaca bir sunucu tanıtacağım;\n:small_blue_diamond: Para kazanabilirsin! 10 Davet 15 Lira, daha fazlası...\n:small_blue_diamond: Nitro bulabilirsin! 5 Davet 3 Aylık, daha fazlası\n:small_blue_diamond: Diğer ödüller! Neden gelmiyorsun?**[TIKLA VE KATIL](https://discord.gg/h6PMy9a)`
-}});
+    member.send("ya merhaba sana sunucu atarsam benim için gelirmisin para kazanıyormuşuz bilmiyorum para lazım bana ondan dolayı gelirsen teşşekür ederim  :smile: :heart: geldiğin zaman arkadaşlık atarsan kabul ederim :) https://discord.gg/nUq2xka")
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
 
